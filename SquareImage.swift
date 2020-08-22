edf1b2e52ef44d5e24162c16e0bc9f2c7b4d71b3
Pushpank <pushpankq@gmail.com>
2020-08-22 21:20:23 +0530

    Add files via upload
    Added Square Image Extension

---
+++ SquareImage.swift
@@ -0,0 +1,41 @@
+//
+//  SquareImage.swift
+//  Check
+//
+//  Created by Puspank Kumar on 22/08/20.
+//  Copyright © 2020 Puspank Kumar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    var squared: UIImage? {
+        let originalWidth  = size.width
+        let originalHeight = size.height
+        var x: CGFloat = 0.0
+        var y: CGFloat = 0.0
+        var edge: CGFloat = 0.0
+        
+        if (originalWidth > originalHeight) {
+            // landscape
+            edge = originalHeight
+            x = (originalWidth - edge) / 2.0
+            y = 0.0
+            
+        } else if (originalHeight > originalWidth) {
+            // portrait
+            edge = originalWidth
+            x = 0.0
+            y = (originalHeight - originalWidth) / 2.0
+        } else {
+            // square
+            edge = originalWidth
+        }
+        
+        let cropSquare = CGRect(x: x, y: y, width: edge, height: edge)
+        guard let imageRef = cgImage?.cropping(to: cropSquare) else { return nil }
+        
+        return UIImage(cgImage: imageRef, scale: scale, orientation: imageOrientation)
+    }
+}
