a4f8660a81c996e49bc0fa970f133098dd132f71
Pushpank <pushpankq@gmail.com>
2020-08-22 21:39:07 +0530

    Add files via upload
    Image resized

--- SquareImage.swift
+++ SquareImage.swift
@@ -38,4 +38,22 @@ extension UIImage {
         
         return UIImage(cgImage: imageRef, scale: scale, orientation: imageOrientation)
     }
+    
+    func resized(maxSize: CGFloat) -> UIImage? {
+        let scale: CGFloat
+        if size.width > size.height {
+            scale = maxSize / size.width
+        }
+        else {
+            scale = maxSize / size.height
+        }
+        
+        let newWidth = size.width * scale
+        let newHeight = size.height * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 }
